63a29f8f4bc2a5b0fb90b0c5a0a25fa0d7fdfeae
Oleksandr Shpak <kotmultos@gmail.com>
2022-09-22 16:06:36 +0300

    links to images

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,11 +1,11 @@
 export const images = {
     fractal_theory: 'http://drive.google.com/uc?export=view&id=1cUeQ2Bp9DaTOH_s9847EaZVJN8NTIwbc',
-    fractal_dragon: 'http://drive.google.com/uc?export=view&id=1cUeQ2Bp9DaTOH_s9847EaZVJN8NTIwbc',
-    fractal_cesaro: 'http://drive.google.com/uc?export=view&id=1cUeQ2Bp9DaTOH_s9847EaZVJN8NTIwbc',
-    fractal_barnsley: 'http://drive.google.com/uc?export=view&id=1cUeQ2Bp9DaTOH_s9847EaZVJN8NTIwbc',
-    fractal_gilbert_peano: 'http://drive.google.com/uc?export=view&id=1cUeQ2Bp9DaTOH_s9847EaZVJN8NTIwbc',
-    cmyk: 'http://drive.google.com/uc?export=view&id=1cUeQ2Bp9DaTOH_s9847EaZVJN8NTIwbc',
-    hsl: 'http://drive.google.com/uc?export=view&id=1cUeQ2Bp9DaTOH_s9847EaZVJN8NTIwbc',
-    paralelogram: 'http://drive.google.com/uc?export=view&id=1cUeQ2Bp9DaTOH_s9847EaZVJN8NTIwbc',
-    linekx: 'http://drive.google.com/uc?export=view&id=1cUeQ2Bp9DaTOH_s9847EaZVJN8NTIwbc'
+    fractal_dragon: 'http://drive.google.com/uc?export=view&id=1C8OoZGtQ0-fdYlkPr6ssYiRZvA8zt1_v',
+    fractal_cesaro: 'http://drive.google.com/uc?export=view&id=1C8OoZGtQ0-fdYlkPr6ssYiRZvA8zt1_v',
+    fractal_barnsley: 'http://drive.google.com/uc?export=view&id=16y_ley8Cw5tpidw9PcVqtyDtJ8W0s7XS',
+    fractal_gilbert_peano: 'http://drive.google.com/uc?export=view&id=1ukgY1fUL_Nqa-KaEhduis7_EZVDg7sFy',
+    cmyk: 'http://drive.google.com/uc?export=view&id=1TxkrzC9eh3YTpCpUvFIqT4AtunB9UDE4',
+    hsl: 'http://drive.google.com/uc?export=view&id=1C8OoZGtQ0-fdYlkPr6ssYiRZvA8zt1_v',
+    parallelogram: 'http://drive.google.com/uc?export=view&id=1ukgY1fUL_Nqa-KaEhduis7_EZVDg7sFy',
+    linekx: 'http://drive.google.com/uc?export=view&id=1ukgY1fUL_Nqa-KaEhduis7_EZVDg7sFy'
 }
