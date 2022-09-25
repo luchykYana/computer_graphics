8a45f91f0f4570ec0eda1f142fab8a0878f4214a
Yana <yanaluchyk@gmail.com>
2022-09-25 22:33:18 +0300

    old version fractal. Nothing changed

--- src/components/DisplayFractal/DisplayFractal.js
+++ src/components/DisplayFractal/DisplayFractal.js
@@ -42,9 +42,68 @@ const DisplayFractal = ({fractal_header_name, fractal_name, fractal_description,
     }
     const buildBarnsleyFractal = (d) => {
         console.log('buildBarnsleyFractal\nvalue: ' + d)
+        let canvas;
+        let canvasContext;
+
+        let x = 0, y = 0;
+
+        function draw () {
+            canvas = document.getElementById("fractal_canvas");
+            canvasContext = canvas.getContext('2d');
+
+            canvasContext.fillStyle = "white";
+            canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+
+            setInterval(() => {
+                // Update 20 times every frame
+                for (let i = 0; i < 20; i++)
+                    update();
+
+            }, 1000/250); // 250 frames per second
+        };
+
+        function update() {
+
+            let nextX, nextY;
+            let r = Math.random();
+            if (r < 0.01) {
+                nextX =  0;
+                nextY =  0.16 * y;
+            } else if (r < 0.86) {
+                nextX =  0.85 * x + 0.04 * y;
+                nextY = -0.04 * x + 0.85 * y + 1.6;
+            } else if (r < 0.93) {
+                nextX =  0.20 * x - 0.26 * y;
+                nextY =  0.23 * x + 0.22 * y + 1.6;
+            } else {
+                nextX = -0.15 * x + 0.28 * y;
+                nextY =  0.26 * x + 0.24 * y + 0.44;
+            }
+
+            // Scaling and positioning
+            let plotX = canvas.width * (x + 3) / 6;
+            let plotY = canvas.height - canvas.height * ((y + 2) / 14);
+
+            drawFilledCircle(plotX, plotY, 1, "green");
+
+            x = nextX;
+            y = nextY;
+
+        }
+        const drawFilledCircle = (centerX, centerY, radius, color) => {
+            canvasContext.beginPath();
+            canvasContext.fillStyle = color;
+            canvasContext.arc(centerX, centerY, radius, 0, 2 * Math.PI, true);
+            canvasContext.fill();
+        };
+
+        draw();
     }
     const buildCesaroFractal = (d) => {
-        console.log('buildCesaroFractal\nvalue: ' + d)
+        console.log('buildCesaroFractal\nvalue: ' + d);
+
+
+
     }
     const buildGilbertFractal = (d) => {
         function Drawing(dx, dy)//функція формування лінії
