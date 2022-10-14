c84de0f203e66eaee7632504d36e280ef77acac5
Oleksandr Shpak <kotmultos@gmail.com>
2022-10-14 17:43:18 +0300

    color models theory

--- src/components/ColorModelsTheory/ColorModelsTheory.js
+++ src/components/ColorModelsTheory/ColorModelsTheory.js
@@ -1,21 +1,32 @@
 import {Title} from '../Title/Title';
 import {icons, images} from '../../constants';
 
+import './ColorModelTheory.css'
+
 const ColorModelsTheory = () => {
     return (
         <div className={'content'}>
             <Title icon_name={icons.book} caption={'CMYK та HSL — колірні моделі?'}/>
 
-            <div className={'flex grid'} >
-                <div className={'fractal-theory-text'}>
-                    <p>Слово фрактал утворено від латинського fractus і в перекладі означає той, що складається з фрагментів (подрібнений).</p>
-                    <br/>
-                    <p>Фракталом називається структура, що складається з частин, які в якомусь сенсі подібні до цілого.</p>
-                    <br/>
-                    <p>В основі цього явища лежить дуже проста ідея: нескінчену по красі і різноманітності множину фігур можна отримати з відносно простих конструкцій за допомогою всього двох операцій - копіювання і масштабування.</p>
+            <div className={"flex grid mt-10"}>
+                <div className={"flex text-theory"}>
+                    <p><strong>CMYK</strong> — субтрактивна колірна модель, використовується у поліграфії, перш за все при
+                        багатофарбовому друці. Українською перші три кольори називають так: блакитний, пурпуровий, жовтий; але професіонали
+                        мають на увазі ціан, маджента та жовтий.</p>
                 </div>
                 <div>
-                    <img id='fractal_theory' src={images.fractal_theory} alt="fractal"/>
+                    <img className={"img-theory"} src={images.cmyk} alt="cmyk model"/>
+                </div>
+            </div>
+
+            <div className={"flex grid"}>
+                <div className={"flex text-theory"}>
+                    <p><strong>HSL</strong> — колірна модель, в якій будь-який колір визначається трьома характеристиками: кольоровим тоном,
+                        наприклад, синім, червоним, жовтим тощо; насиченістю, тобто частиною чистого кольору, без домішки
+                        чорної та білої фарб; «світністю», тобто близькістю до білого кольору. Схожа на колірну модель HSV.</p>
+                </div>
+                <div>
+                    <img className={"img-theory"} src={images.hsl} alt="hsl model"/>
                 </div>
             </div>
         </div>
