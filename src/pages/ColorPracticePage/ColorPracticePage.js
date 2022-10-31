9fddefc11f18dba6efe20f66fb9e01fb584c7171
Yana <yanaluchyk@gmail.com>
2022-10-31 22:22:17 +0200

    convert rgbToHsl and hslToRgb

--- src/pages/ColorPracticePage/ColorPracticePage.js
+++ src/pages/ColorPracticePage/ColorPracticePage.js
@@ -2,37 +2,37 @@ import {Title} from '../../components';
 import {icons} from '../../constants';
 
 import css from './ColorPractisePage.module.css';
-import {useEffect} from "react";
 
 const ConvertRGBtoHSL = (r, g, b) => {
     [r, g, b] = [r / 255, g / 255, b / 255];
     let max = Math.max(r, g, b);
     let min = Math.min(r, g, b);
-    let [h, s, l] = [0, 0, 0];
+    let h, s, l;
 
     if (max === min) {
         h = 0;
     } else if (max === r && g >= b) {
-        h = Math.round(60 * (g - b) / (max - min));
+        h = 60 * (g - b) / (max - min);
     } else if (max === r && g < b) {
-        h = Math.round(60 * (g - b) / (max - min) + 360);
+        h = 60 * (g - b) / (max - min) + 360;
     } else if (max === g) {
-        h = Math.round(60 * (b - r) / (max - min) + 120);
-    } else {
-        h = Math.round(60 * (r - g) / (max - min) + 240);
+        h = 60 * (b - r) / (max - min) + 120;
+    } else if (max === b) {
+        h = 60 * ((r - g) / (max - min)) + 240;
     }
 
-    l = Math.round((max + min) * 0.5);
+    l = (max + min) / 2;
 
     if (l === 0 || max === min) {
         s = 0;
     } else if (l > 0 && l < 0.5) {
         s = (max - min) / (2 * l);
-    } else {
-        s = (max - min) / (2 - 2 * l);
+    } else if (l > 0.5 && l !== 1) {
+        s = (max - min) / (2 - (max + min));
     }
 
-    return [h, s, l];
+    // return [Math.round(h), Number(s).toFixed(2), Number(l).toFixed(2)];
+    return [Math.round(h), 0, l];
 }
 
 const ConvertHSLtoRGB = (h, s, l) => {
@@ -62,7 +62,7 @@ const ConvertHSLtoRGB = (h, s, l) => {
         r = x;
         g = 0;
         b = c;
-    } else {
+    } else if (h >= 300 && h < 360) {
         r = c;
         g = 0;
         b = x;
@@ -71,58 +71,39 @@ const ConvertHSLtoRGB = (h, s, l) => {
     r = (r + m) * 255;
     g = (g + m) * 255;
     b = (b + m) * 255;
-
-    return [r, g, b];
+    // console.log('r: ' + r + ' g: ' + g + ' b: ' + b);
+    return [Math.round(r), Math.round(g), Math.round(b)];
+    // return [200, 200, 200];
 }
 
 const ColorPracticePage = () => {
-    // const [imageLink, setImageLink] = useEffect();
-
-
     const loadPhoto = () => {
         let canvas_rgb = document.getElementById('rgb_canvas');
-        // let canvas_hsl = document.getElementById('hsl_canvas');
-        // let canvas_cmyk = document.getElementById('cmyk_canvas');
-        let context_rgb = canvas_rgb.getContext('2d');
-        // let context_hsl = canvas_hsl.getContext('2d');
-        // let context_cmyk = canvas_cmyk.getContext('2d');
-
-        make_image();
-
-        function make_image() {
-            let base_image = new Image();
-
-            // base_image.src = icons.img + "?not-from-cache-please";
-            base_image.src = icons.img;
-            // base_image.src = imageLink;
-            // base_image.width = 20;
-            base_image.onload = () => {
-                context_rgb.drawImage(base_image, 0, 0, canvas_rgb.width, canvas_rgb.height);
-            }
-            base_image.setAttribute('crossOrigin', '');
-
-            // context_hsl.drawImage(base_image, 0, 0, canvas_hsl.width, canvas_hsl.height);
-            // context_cmyk.drawImage(base_image, 0, 0, canvas_cmyk.width, canvas_cmyk.height);
-        }
-
+        let context_rgb = canvas_rgb.getContext('2d')
         let imageData = context_rgb.getImageData(0, 0, canvas_rgb.width, canvas_rgb.height);
-
-        // let data = imageData.data;
-        // console.log(data);
 
         editPixels(imageData.data);
         drawEditedImage(imageData);
 
         function editPixels(imgData) {
-            for (let i = 0; i < imgData.length; i += 3) {
-                [imageData[i], imageData[i + 1], imageData[i + 2]] = ConvertRGBtoHSL(imgData[i], imgData[i + 1], imgData[i + 2]);
-                [imageData[i], imageData[i + 1], imageData[i + 2]] = ConvertHSLtoRGB(imgData[i], imgData[i + 1], imgData[i + 2]);
+            console.log(imageData.data);
+            let mas1 = [0, 0, 0];
+            let mas2 = [0, 0, 0];
+            for (let i = 0; i < imgData.length; i += 4) {
+                mas1 = ConvertRGBtoHSL(imgData[i], imgData[i + 1], imgData[i + 2]);
+                mas2 = ConvertHSLtoRGB(mas1[0], mas1[1], mas1[2]);
+                imageData[i] = mas2[0];
+                imageData[i + 1] = mas2[1];
+                imageData[i + 2] = mas2[2];
             }
+            console.log(imageData);
         }
 
         function drawEditedImage(newData) {
             let canvasEdited = document.getElementById("hsl_canvas");
             let ctxEdited = canvasEdited.getContext('2d');
+            canvasEdited.width = 300;
+            canvasEdited.height = 180;
             ctxEdited.putImageData(newData, 0, 0);
         }
     }
@@ -152,7 +133,7 @@ const ColorPracticePage = () => {
                 <canvas id={'rgb_canvas'} className={`${css.colorCanvas}`} width="300" height="180"></canvas>
                 <canvas id={'cmyk_canvas'} className={`${css.colorCanvas}`} width="300" height="180"></canvas>
                 <canvas id={'hsl_canvas'} className={`${css.colorCanvas}`} width="300" height="180"></canvas>
-                <button onClick={loadPhoto}>Вибрати фото</button>
+                <button onClick={loadPhoto} id={'loadPhoto'}>Вибрати фото</button>
 
                 <input onChange={ImageChange} id={'myInput'} name={'fileName'} type="file"/>
             </div>
