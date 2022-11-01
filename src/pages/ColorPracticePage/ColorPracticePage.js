fd4876d78cfe7de4244d7024f50b1a9bf788b054
Oleksandr Shpak <kotmultos@gmail.com>
2022-11-01 17:58:03 +0200

    change lightness

--- src/pages/ColorPracticePage/ColorPracticePage.js
+++ src/pages/ColorPracticePage/ColorPracticePage.js
@@ -208,11 +208,17 @@ const ColorPracticePage = () => {
         let minRange, maxRange;
 
         if(document.getElementById('red').checked === true) {
-            minRange = 300; maxRange= 60;
+            minRange = 0; maxRange= 60;
+        } else if (document.getElementById('yellow').checked === true) {
+            minRange = 60; maxRange= 120;
         } else if (document.getElementById('green').checked === true) {
-            minRange = 60; maxRange= 180;
+            minRange = 120; maxRange= 180;
+        } else if (document.getElementById('cyan').checked === true) {
+            minRange = 180; maxRange= 240;
         } else if (document.getElementById('blue').checked === true) {
-            minRange = 180; maxRange= 300;
+            minRange = 240; maxRange= 300;
+        } else if (document.getElementById('magenta').checked === true) {
+            minRange = 300; maxRange= 360;
         }
 
         let canvas_cmyk = document.getElementById('cmyk_canvas');
@@ -220,11 +226,11 @@ const ColorPracticePage = () => {
 
         let imageData = context_cmyk.getImageData(0, 0, canvas_cmyk.width, canvas_cmyk.height);
 
-        editPixelsHSL(imageData.data);
+        editPixelsChangeSaturation(imageData.data);
         drawEditedImage(imageData, 'hsl_canvas');
 
 
-        function editPixelsHSL(imgData) {
+        function editPixelsChangeSaturation(imgData) {
             let mas1 = [0, 0, 0];
             let mas2 = [0, 0, 0];
             for (let i = 0; i < imgData.length; i += 4) {
@@ -260,8 +266,80 @@ const ColorPracticePage = () => {
             canvasEdited.height = imageHeight;
             ctxEdited.putImageData(newData, 0, 0);
         }
+    }
+
+    const onLightnessChange = (event) => {
+        document.getElementById('lightnessValue').value = event.target.value;
+        const value = event.target.value;
+
+        let minRange, maxRange;
+
+        if(document.getElementById('red').checked === true) {
+            minRange = 0; maxRange= 60;
+        } else if (document.getElementById('yellow').checked === true) {
+            minRange = 60; maxRange= 120;
+        } else if (document.getElementById('green').checked === true) {
+            minRange = 120; maxRange= 180;
+        } else if (document.getElementById('cyan').checked === true) {
+            minRange = 180; maxRange= 240;
+        } else if (document.getElementById('blue').checked === true) {
+            minRange = 240; maxRange= 300;
+        } else if (document.getElementById('magenta').checked === true) {
+            minRange = 300; maxRange= 360;
+        }
+
+        let canvas_cmyk = document.getElementById('cmyk_canvas');
+        let context_cmyk = canvas_cmyk.getContext('2d')
+
+        let imageData = context_cmyk.getImageData(0, 0, canvas_cmyk.width, canvas_cmyk.height);
+
+        editPixelsChangeLightness(imageData.data);
+        drawEditedImage(imageData, 'hsl_canvas');
 
 
+        function editPixelsChangeLightness(imgData) {
+            let mas1 = [0, 0, 0];
+            let mas2 = [0, 0, 0];
+            for (let i = 0; i < imgData.length; i += 4) {
+                // get hsl values
+                mas1 = ConvertRGBtoHSL(imgData[i], imgData[i + 1], imgData[i + 2]);
+                const h = mas1[0];
+                if (h >= minRange && h <= maxRange) {
+                    // console.log(`${mas1[0]} ${mas1[1]} ${mas1[2]}`)
+                    let l = mas1[2];
+                    // console.log(s)
+                    l += (value / 100);
+                    if (l > 1) l = 1;
+                    if (l < 0) l = 0;
+
+                    // console.log(s)
+
+                    mas2 = ConvertHSLtoRGB(mas1[0], mas1[1], l);
+                    imageData.data[i] = mas2[0];
+                    imageData.data[i + 1] = mas2[1];
+                    imageData.data[i + 2] = mas2[2];
+
+                    // console.log(`${mas2[0]} ${mas2[1]} ${mas2[2]}`)
+
+                    // break;
+                }
+            }
+        }
+
+        function drawEditedImage(newData, canvasID) {
+            let canvasEdited = document.getElementById(canvasID);
+            let ctxEdited = canvasEdited.getContext('2d');
+            canvasEdited.width = imageWidth;
+            canvasEdited.height = imageHeight;
+            ctxEdited.putImageData(newData, 0, 0);
+        }
+    }
+
+    const resetValues = (e) => {
+        document.getElementById('lightness').value = 0;
+        // document.getElementById('lightness').onchange;
+        document.getElementById('saturation').value = 0;
+        // document.getElementById('saturation').onchange;
     }
 
     return (
@@ -356,14 +434,24 @@ const ColorPracticePage = () => {
             </div>
             <input id={'red'} type="radio" name={'saturationRadio'} value={'red'} defaultChecked={true}/>
             <label htmlFor='red'>red</label>
+            <input id={'yellow'} type="radio" name={'saturationRadio'} value={'yellow'} />
+            <label htmlFor='yellow'>yellow</label>
             <input id={'green'} type="radio" name={'saturationRadio'} value={'green'} />
             <label htmlFor='green'>green</label>
+            <input id={'cyan'} type="radio" name={'saturationRadio'} value={'cyan'} />
+            <label htmlFor='cyan'>cyan</label>
             <input id={'blue'} type="radio" name={'saturationRadio'} value={'blue'} />
             <label htmlFor='blue'>blue</label>
-
+            <input id={'magenta'} type="radio" name={'saturationRadio'} value={'magenta'} />
+            <label htmlFor='magenta'>magenta</label>
 
             <input onChange={onSaturationChange} id={'saturation'} type="range" name='saturation' defaultValue={0} min={-100} max={100}/>
             <input  defaultValue={0} type="text" id={'saturationValue'}/>
+
+            <input onChange={onLightnessChange} id={'lightness'} type="range" name='lightness' defaultValue={0} min={-100} max={100}/>
+            <input  defaultValue={0} type="text" id={'lightnessValue'}/>
+
+            <button onClick={resetValues} >Reset</button>
 
         </div>
     );
