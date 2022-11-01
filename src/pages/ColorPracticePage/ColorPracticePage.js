590188618e25943123686fd1653936b4d289d4c0
Yana <yanaluchyk@gmail.com>
2022-11-01 21:46:28 +0200

    pipette

--- src/pages/ColorPracticePage/ColorPracticePage.js
+++ src/pages/ColorPracticePage/ColorPracticePage.js
@@ -11,6 +11,7 @@ const ColorPracticePage = () => {
     const imageWidth = 400;
 
     const [isImageSet, setIsImageSet] = useState(false);
+    const [mousePos, setMousePos] = useState({x: 0, y: 0});
 
     function drawEditedImage(newData, canvasID) {
         let canvasEdited = document.getElementById(canvasID);
@@ -102,18 +103,24 @@ const ColorPracticePage = () => {
     const getRangeOfValues = () => {
         let min, max;
 
-        if(document.getElementById('red').checked === true) {
-            min = 0; max= 60;
+        if (document.getElementById('red').checked === true) {
+            min = 0;
+            max = 60;
         } else if (document.getElementById('yellow').checked === true) {
-            min = 60; max= 120;
+            min = 60;
+            max = 120;
         } else if (document.getElementById('green').checked === true) {
-            min = 120; max= 180;
+            min = 120;
+            max = 180;
         } else if (document.getElementById('cyan').checked === true) {
-            min = 180; max= 240;
+            min = 180;
+            max = 240;
         } else if (document.getElementById('blue').checked === true) {
-            min = 240; max= 300;
+            min = 240;
+            max = 300;
         } else if (document.getElementById('magenta').checked === true) {
-            min = 300; max= 360;
+            min = 300;
+            max = 360;
         }
 
         return [min, max]
@@ -190,75 +197,150 @@ const ColorPracticePage = () => {
         let nativeInputValueSetter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, "value").set;
         nativeInputValueSetter.call(document.getElementById('lightness'), 0);
 
-        let ev1 = new Event('input', { bubbles: true});
+        let ev1 = new Event('input', {bubbles: true});
         document.getElementById('lightness').dispatchEvent(ev1);
 
         nativeInputValueSetter.call(document.getElementById('saturation'), 0);
 
-        let ev2 = new Event('input', { bubbles: true});
+        let ev2 = new Event('input', {bubbles: true});
         document.getElementById('saturation').dispatchEvent(ev2);
     }
 
+    const getMousePosition = (canvas, e) => {
+        let rect = canvas.getBoundingClientRect();
+        return {
+            x: Math.round(e.clientX - rect.left),
+            y: Math.round(e.clientY - rect.top)
+        };
+    }
+    const getMousePositionAll = (e) => {
+        setMousePos({x: e.clientX, y: e.clientY});
+    }
+
+    const changePipe1 = (cmyk, rgb) => {
+        const pipette = document.getElementById('pipette1');
+        const pipette_text = document.getElementById('pipette1_text');
+        const pipette_img = document.getElementById('pipette1_img_fill');
+        pipette.style.top = `${mousePos.y - 90}px`;
+        pipette.style.left = `${mousePos.x + 5}px`;
+        pipette.style.display = 'block';
+        pipette_text.innerText = `CMYK ( ${(cmyk[0] * 100).toFixed(0)}; ${(cmyk[1] * 100).toFixed(0)}; ${(cmyk[2] * 100).toFixed(0)}; ${(cmyk[3] * 100).toFixed(0)} )`;
+        pipette_img.style.backgroundColor = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`
+    }
+    const changePipe2 = (hsl, rgb) => {
+        const pipette = document.getElementById('pipette2');
+        const pipette_text = document.getElementById('pipette2_text');
+        const pipette_img = document.getElementById('pipette2_img_fill');
+        pipette.style.top = `${mousePos.y - 90}px`;
+        pipette.style.left = `${mousePos.x + 5}px`;
+        pipette.style.display = 'block';
+        pipette_text.innerText = `HSL ( ${(hsl[0])}; ${(hsl[1] * 100).toFixed(0)}; ${(hsl[2] * 100).toFixed(0)} )`;
+        pipette_img.style.backgroundColor = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`
+    }
+
+    const mouseMove = (e) => {
+        let {x, y} = getMousePosition(e.target, e);
+        const imageData = e.target.getContext('2d').getImageData(0, 0, e.target.width, e.target.height).data;
+        const index = (e.target.width * (y - 1) + x) * 4;
+        const rgb = [imageData[index], imageData[index + 1], imageData[index + 2]];
+        const cmyk = modelFunc.RGBtoCMYK(rgb[0], rgb[1], rgb[2]);
+        const hsl = modelFunc.RGBtoHSL(rgb[0], rgb[1], rgb[2]);
+
+        if (isImageSet === true) {
+            if (e.target.id === 'cmyk_canvas') {
+                changePipe1(cmyk, rgb);
+            }
+            if (e.target.id === 'hsl_canvas') {
+                changePipe2(hsl, rgb);
+            }
+        }
+
+        return {cmyk, hsl, rgb};
+    }
+
+    const disappearPipe1 = () => document.getElementById('pipette1').style.display = 'none';
+    const disappearPipe2 = () => document.getElementById('pipette2').style.display = 'none';
+
+    const changeParams1 = (cmyk, rgb) => {
+        document.getElementById('CMYK').style.backgroundColor = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
+        document.getElementById('C').innerText = `${(cmyk[0] * 100).toFixed(0)}`;
+        document.getElementById('M').innerText = `${(cmyk[1] * 100).toFixed(0)}`;
+        document.getElementById('Y').innerText = `${(cmyk[2] * 100).toFixed(0)}`;
+        document.getElementById('K').innerText = `${(cmyk[3] * 100).toFixed(0)}`;
+    }
+    const changeParams2 = (hsl, rgb) => {
+        document.getElementById('HSL').style.backgroundColor = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
+        document.getElementById('H').innerText = `${hsl[0]}`;
+        document.getElementById('S').innerText = `${(hsl[1] * 100).toFixed(0)}`;
+        document.getElementById('L').innerText = `${(hsl[2] * 100).toFixed(0)}`;
+    }
+
+    const clickPipe = (e) => {
+        const {cmyk, hsl, rgb} = mouseMove(e);
+
+        if (isImageSet === true) {
+            if (e.target.id === 'cmyk_canvas') {
+                changeParams1(cmyk, rgb);
+            }
+            if (e.target.id === 'hsl_canvas') {
+                changeParams2(hsl, rgb);
+            }
+        }
+    }
+
+
     return (
-        <div className={`${css.content}`}>
+        <div className={`${css.content}`} onMouseMove={getMousePositionAll}>
             <Title icon_name={icons.brush} caption={'Перетворення моделей'}/>
 
             <div className={`${css.flex} ${css.height} ${css.gap}`}>
                 <div>
                     <div className={`${css.flex} ${css.colorLetters}`}>
-
                         <div className={`${css.mainLetters} ${css.flex}`}>
                             <div className={`${css.mbMain}`}><h2>CMYK</h2></div>
-                            <div className={`${css.pixelInfo2}`}></div>
+                            <div className={`${css.pixelInfo2}`} id={'CMYK'}></div>
                         </div>
                         <div className={`${css.sLetters}`}>
                             <div className={`${css.flex} ${css.letterGroupGap2}`}>
                                 <div className={`${css.flex} ${css.letterGap} ${css.cmykWidthLetter}`}>
                                     <h3>(Cyan) Блакитний</h3>
-                                    <div className={`${css.pixelInfo}`}></div>
+                                    <div className={`${css.pixelInfo}`} id={'C'}></div>
                                 </div>
-
                                 <div className={`${css.flex} ${css.letterGap} ${css.cmykWidthLetter}`}>
                                     <h3>(Magenta) Пурпуровий</h3>
-                                    <div className={`${css.pixelInfo}`}></div>
+                                    <div className={`${css.pixelInfo}`} id={'M'}></div>
                                 </div>
                             </div>
-
                             <div className={`${css.flex} ${css.letterGroupGap2}`}>
                                 <div className={`${css.flex} ${css.letterGap} ${css.cmykWidthLetter}`}>
                                     <h3>(Yellow) Жовтий</h3>
-                                    <div className={`${css.pixelInfo}`}></div>
+                                    <div className={`${css.pixelInfo}`} id={'Y'}></div>
                                 </div>
-
                                 <div className={`${css.flex} ${css.letterGap} ${css.cmykWidthLetter}`}>
                                     <h3>(Key) Чорний</h3>
-                                    <div className={`${css.pixelInfo}`}></div>
+                                    <div className={`${css.pixelInfo}`} id={'K'}></div>
                                 </div>
                             </div>
                         </div>
-
                     </div>
                     <div className={`${css.flex} ${css.colorLetters}`}>
-
                         <div className={`${css.mainLetters} ${css.flex}`}>
                             <div className={`${css.mbMain}`}><h2>HSL</h2></div>
-                            <div className={`${css.pixelInfo2}`}></div>
+                            <div className={`${css.pixelInfo2}`} id={'HSL'}></div>
                         </div>
                         <div className={`${css.flex} ${css.letterGroupGap}`}>
-
                             <div className={`${css.flex} ${css.letterGap} ${css.hslWidthLetter}`}>
                                 <h3>(Hue) Відтінок</h3>
-                                <div className={`${css.pixelInfo}`}></div>
+                                <div className={`${css.pixelInfo}`} id={'H'}></div>
                             </div>
                             <div className={`${css.flex} ${css.letterGap} ${css.hslWidthLetter}`}>
                                 <h3>(Saturation) Насиченість</h3>
-                                <div className={`${css.pixelInfo}`}></div>
+                                <div className={`${css.pixelInfo}`} id={'S'}></div>
                             </div>
                             <div className={`${css.flex} ${css.letterGap} ${css.hslWidthLetter}`}>
                                 <h3>(Lightness) Світлота</h3>
-                                <div className={`${css.pixelInfo}`}></div>
+                                <div className={`${css.pixelInfo}`} id={'L'}></div>
                             </div>
-
                         </div>
 
                     </div>
@@ -267,21 +349,42 @@ const ColorPracticePage = () => {
                 <div>
                     <div>
                         <canvas id={'cmyk_canvas'} className={`${css.colorCanvas}`} width={imageWidth}
-                                height={imageHeight}></canvas>
+                                height={imageHeight} onMouseMove={mouseMove} onMouseLeave={disappearPipe1}
+                                onClick={clickPipe}>
+                        </canvas>
                         {!isImageSet && <img onClick={buttonClick} className={`${css.uploadButton}`} src={icons.upload}
                                              alt="upload"/>}
                         {isImageSet &&
                             <img onClick={buttonDeleteClick} className={`${css.deleteButton}`} src={icons.trash}
                                  alt="delete"/>}
                     </div>
+
+                    <div className={`${css.pipette}`} id={'pipette1'}>
+                        <div className={`${css.pipette_text}`} id={'pipette1_text'}></div>
+                        <div>
+                            <div className={`${css.pipette_img_fill}`} id={'pipette1_img_fill'}></div>
+                            <img src={icons.pipette_2} alt="pipette"/>
+                        </div>
+                    </div>
+
                     <div>
                         <canvas id={'hsl_canvas'} className={`${css.colorCanvas}`} width={imageWidth}
-                                height={imageHeight}></canvas>
+                                height={imageHeight} onMouseMove={mouseMove} onMouseLeave={disappearPipe2}
+                                onClick={clickPipe}>
+                        </canvas>
                         {!isImageSet && <img onClick={buttonClick} className={`${css.uploadButton}`} src={icons.upload}
                                              alt="upload"/>}
                         {isImageSet &&
                             <img onClick={buttonDeleteClick} className={`${css.deleteButton}`} src={icons.trash}
                                  alt="delete"/>}
+                    </div>
+
+                    <div className={`${css.pipette}`} id={'pipette2'}>
+                        <div className={`${css.pipette_text}`} id={'pipette2_text'}></div>
+                        <div>
+                            <div className={`${css.pipette_img_fill}`} id={'pipette2_img_fill'}></div>
+                            <img src={icons.pipette_2} alt="pipette2"/>
+                        </div>
                     </div>
                 </div>
 
@@ -289,27 +392,29 @@ const ColorPracticePage = () => {
                     event.target.value = null;
                 }} onChange={ImageChange} id={'myInput'} name={'fileName'} type="file"/>
             </div>
-            <input onChange={resetValues} id={'red'} type="radio" name={'saturationRadio'} value={'red'} defaultChecked={true}/>
+            <input onChange={resetValues} id={'red'} type="radio" name={'saturationRadio'} value={'red'}
+                   defaultChecked={true}/>
             <label htmlFor='red'>red</label>
-            <input onChange={resetValues} id={'yellow'} type="radio" name={'saturationRadio'} value={'yellow'} />
+            <input onChange={resetValues} id={'yellow'} type="radio" name={'saturationRadio'} value={'yellow'}/>
             <label htmlFor='yellow'>yellow</label>
-            <input onChange={resetValues} id={'green'} type="radio" name={'saturationRadio'} value={'green'} />
+            <input onChange={resetValues} id={'green'} type="radio" name={'saturationRadio'} value={'green'}/>
             <label htmlFor='green'>green</label>
-            <input onChange={resetValues} id={'cyan'} type="radio" name={'saturationRadio'} value={'cyan'} />
+            <input onChange={resetValues} id={'cyan'} type="radio" name={'saturationRadio'} value={'cyan'}/>
             <label htmlFor='cyan'>cyan</label>
-            <input onChange={resetValues} id={'blue'} type="radio" name={'saturationRadio'} value={'blue'} />
+            <input onChange={resetValues} id={'blue'} type="radio" name={'saturationRadio'} value={'blue'}/>
             <label htmlFor='blue'>blue</label>
-            <input onChange={resetValues} id={'magenta'} type="radio" name={'saturationRadio'} value={'magenta'} />
+            <input onChange={resetValues} id={'magenta'} type="radio" name={'saturationRadio'} value={'magenta'}/>
             <label htmlFor='magenta'>magenta</label>
 
             <input onChange={onSaturationChange} id={'saturation'} type="range" name='saturation' defaultValue={0}
                    min={-100} max={100}/>
             <input defaultValue={0} type="text" id={'saturationValue'}/>
 
-            <input onChange={onLightnessChange} id={'lightness'} type="range" name='lightness' defaultValue={0} min={-100} max={100}/>
-            <input  defaultValue={0} type="text" id={'lightnessValue'}/>
+            <input onChange={onLightnessChange} id={'lightness'} type="range" name='lightness' defaultValue={0}
+                   min={-100} max={100}/>
+            <input defaultValue={0} type="text" id={'lightnessValue'}/>
 
-            <button onClick={resetValues} >Reset</button>
+            <button onClick={resetValues}>Reset</button>
 
         </div>
     );
