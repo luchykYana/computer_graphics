0f026c5b55dee0ad7b6e5e7cf319d278fc13eed6
Oleksandr Shpak <kotmultos@gmail.com>
2022-11-12 17:01:08 +0200

    coordinate axis

--- src/pages/MovePracticePage/MovePracticePage.js
+++ src/pages/MovePracticePage/MovePracticePage.js
@@ -2,9 +2,7 @@ import {Title} from '../../components';
 import {icons} from '../../constants';
 
 import css from './MovePractisePage.module.css'
-import {InputBar} from '../../components/InputBar/InputBar';
 import {useEffect, useState} from 'react';
-
 
 const MovePracticePage = () => {
     const range = 10;
@@ -30,12 +28,10 @@ const MovePracticePage = () => {
         setPoint4({x: 0, y: 0})
         setLine({k: 0, x: 0})
 
-
         let arr = document.getElementsByTagName('input');
         for(let item of arr) {
             item.value = '';
         }
-
     }
 
     const onMovementChange = () => {
@@ -60,6 +56,153 @@ const MovePracticePage = () => {
         const a = document.getElementById('movement_canvas_link');
         a.href = canvas.toDataURL('image/png');
         a.click();
+    }
+
+    const draw_xy_graph = () => {
+        console.log('works')
+        var grid_size = 25;
+        var x_axis_starting_point = { number: 1, suffix: '' };
+        var y_axis_starting_point = { number: 1, suffix: '' };
+
+        const canvas = document.getElementById('movement_canvas');
+        const ctx = canvas.getContext('2d');
+
+        // canvas width
+        var canvas_width = canvas.width;
+
+        // canvas height
+        var canvas_height = canvas.height;
+
+        // no of vertical grid lines
+        var num_lines_x = Math.floor(canvas_height/grid_size);
+
+        // no of horizontal grid lines
+        var num_lines_y = Math.floor(canvas_width/grid_size);
+
+
+        var x_axis_distance_grid_lines = Math.floor(num_lines_x/2);
+        var y_axis_distance_grid_lines = Math.floor(num_lines_y/2);
+
+
+        // Draw grid lines along X-axis
+        for(let i=0; i<=num_lines_x; i++) {
+            ctx.beginPath();
+            ctx.lineWidth = 1;
+
+            // If line represents X-axis draw in different color
+            if(i === x_axis_distance_grid_lines)
+                ctx.strokeStyle = "#000000";
+            else
+                ctx.strokeStyle = "#e9e9e9";
+
+            if(i === num_lines_x) {
+                ctx.moveTo(0, grid_size*i);
+                ctx.lineTo(canvas_width, grid_size*i);
+            }
+            else {
+                ctx.moveTo(0, grid_size*i+0.5);
+                ctx.lineTo(canvas_width, grid_size*i+0.5);
+            }
+            ctx.stroke();
+        }
+
+        // Draw grid lines along Y-axis
+        for(let i=0; i<=num_lines_y; i++) {
+            ctx.beginPath();
+            ctx.lineWidth = 1;
+
+            // If line represents Y-axis draw in different color
+            if(i == y_axis_distance_grid_lines)
+                ctx.strokeStyle = "#000000";
+            else
+                ctx.strokeStyle = "#e9e9e9";
+
+            if(i == num_lines_y) {
+                ctx.moveTo(grid_size*i, 0);
+                ctx.lineTo(grid_size*i, canvas_height);
+            }
+            else {
+                ctx.moveTo(grid_size*i+0.5, 0);
+                ctx.lineTo(grid_size*i+0.5, canvas_height);
+            }
+            ctx.stroke();
+        }
+
+        ctx.translate(y_axis_distance_grid_lines*grid_size, x_axis_distance_grid_lines*grid_size);
+
+        // Ticks marks along the positive X-axis
+        for(let i=1; i<(num_lines_y - y_axis_distance_grid_lines); i++) {
+            ctx.beginPath();
+            ctx.lineWidth = 1;
+            ctx.strokeStyle = "#000000";
+
+            // Draw a tick mark 6px long (-3 to 3)
+            ctx.moveTo(grid_size*i+0.5, -3);
+            ctx.lineTo(grid_size*i+0.5, 3);
+            ctx.stroke();
+
+            // Text value at that point
+            ctx.font = '9px Arial';
+            ctx.textAlign = 'start';
+            ctx.fillText(x_axis_starting_point.number*i + x_axis_starting_point.suffix, grid_size*i-2, 15);
+        }
+
+// Ticks marks along the negative X-axis
+        for(let i=1; i<y_axis_distance_grid_lines; i++) {
+            ctx.beginPath();
+            ctx.lineWidth = 1;
+            ctx.strokeStyle = "#000000";
+
+            // Draw a tick mark 6px long (-3 to 3)
+            ctx.moveTo(-grid_size*i+0.5, -3);
+            ctx.lineTo(-grid_size*i+0.5, 3);
+            ctx.stroke();
+
+            // Text value at that point
+            ctx.font = '9px Arial';
+            ctx.textAlign = 'end';
+            ctx.fillText(-x_axis_starting_point.number*i + x_axis_starting_point.suffix, -grid_size*i+3, 15);
+        }
+
+        // Ticks marks along the positive Y-axis
+// Positive Y-axis of graph is negative Y-axis of the canvas
+        for(let i=1; i<(num_lines_x - x_axis_distance_grid_lines); i++) {
+            ctx.beginPath();
+            ctx.lineWidth = 1;
+            ctx.strokeStyle = "#000000";
+
+            // Draw a tick mark 6px long (-3 to 3)
+            ctx.moveTo(-3, grid_size*i+0.5);
+            ctx.lineTo(3, grid_size*i+0.5);
+            ctx.stroke();
+
+            // Text value at that point
+            ctx.font = '9px Arial';
+            ctx.textAlign = 'start';
+            ctx.fillText(-y_axis_starting_point.number*i + y_axis_starting_point.suffix, 8, grid_size*i+3);
+        }
+
+// Ticks marks along the negative Y-axis
+// Negative Y-axis of graph is positive Y-axis of the canvas
+        for(let i=1; i<x_axis_distance_grid_lines; i++) {
+            ctx.beginPath();
+            ctx.lineWidth = 1;
+            ctx.strokeStyle = "#000000";
+
+            // Draw a tick mark 6px long (-3 to 3)
+            ctx.moveTo(-3, -grid_size*i+0.5);
+            ctx.lineTo(3, -grid_size*i+0.5);
+            ctx.stroke();
+
+            // Text value at that point
+            ctx.font = '9px Arial';
+            ctx.textAlign = 'start';
+            ctx.fillText(y_axis_starting_point.number*i + y_axis_starting_point.suffix, 8, -grid_size*i+3);
+        }
+
+
+
+
     }
 
     return (
@@ -236,7 +379,7 @@ const MovePracticePage = () => {
 
                 <div>
                     <div>
-                        <canvas height={425} width={425} id={'movement_canvas'} className={`${css.movementCanvas}`}>
+                        <canvas height={425} width={425} onClick={draw_xy_graph} id={'movement_canvas'} className={`${css.movementCanvas}`}>
                         </canvas>
 
                         <div>
