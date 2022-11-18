895ec8fb6243a67da624e48ae457ba2c2d77cbe3
Oleksandr Shpak <kotmultos@gmail.com>
2022-11-18 10:45:20 +0200

    cleaned commented code

--- src/pages/MovePracticePage/MovePracticePage.js
+++ src/pages/MovePracticePage/MovePracticePage.js
@@ -189,19 +189,6 @@ const MovePracticePage = () => {
 
         ctx.stroke();
     }
-    //
-    // const onDrawButtonClick = () => {
-    //     const canvas = document.getElementById('movement_canvas');
-    //     const [ctx, x_axis_distance_grid_lines, y_axis_distance_grid_lines] = repetitiveActions();
-    //
-    //     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    //
-    //     draw_xy_graph();
-    //     draw_line_kx();
-    //     draw_parallelogram(point1, point2,  point3, point4);
-    //
-    //     ctx.translate(-1 * (y_axis_distance_grid_lines * gridSize), -1 * (x_axis_distance_grid_lines * gridSize));
-    // }
 
     const draw_line_kx = () => {
         const canvas = document.getElementById('movement_canvas');
@@ -496,9 +483,6 @@ const MovePracticePage = () => {
                                 </div>
 
                             </div>
-
-                            {/*<button onClick={onDrawButtonClick} className={`${css.button} ${css.margin_left}`}>Пряма*/}
-                            {/*</button>*/}
                         </div>
                     </div>
 
@@ -614,30 +598,9 @@ const MovePracticePage = () => {
                             <div className={`${css.flex} ${css.margin_center}`}>
                                 <button onClick={reset} className={`${css.button}`}>Скидання</button>
                             </div>
-                            {/*<div className={`${css.flex} ${css.margin_center}`}>*/}
-                            {/*    <button onClick={onDrawButtonClick} className={`${css.button}`}>Паралелограм</button>*/}
-                            {/*</div>*/}
-                            {/*<div className={`${css.flex} ${css.margin_center}`}>*/}
-                            {/*    <button onClick={start} className={`${css.button}`}>Рух у часі</button>*/}
-                            {/*</div>*/}
+
                         </div>
                     </div>
-
-                    {/*<div className={`${css.text}`}>*/}
-                    {/*    <p>Рух дзеркального відображення паралелограма:</p>*/}
-                    {/*</div>*/}
-
-                    {/*<div className={`${css.movementRangeBlock}`}>*/}
-                    {/*    /!*<input className={`${css.move}`} onChange={(e) => {*!/*/}
-                    {/*    /!*    onMovementChange(e)*!/*/}
-                    {/*    /!*}} id={'movement_range'}*!/*/}
-                    {/*    /!*       type="range"*!/*/}
-                    {/*    /!*       name='movement_range' defaultValue={0}*!/*/}
-                    {/*    /!*       min={-5} max={5} step={1}/>*!/*/}
-                    {/*    */}
-                    {/*    <button onClick={reset} className={`${css.button}`}>Скидання</button>*/}
-                    {/*</div>*/}
-
                 </div>
 
                 <div>
